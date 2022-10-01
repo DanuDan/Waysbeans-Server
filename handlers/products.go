68a2d5ff7d28ff0074a6651057693f7383ab6060
DanuDan <ardanuaryhfz@gmail.com>
2022-10-01 17:16:08 +0700

    waysbeans

---
+++ handlers/products.go
@@ -0,0 +1,213 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strconv"
+	productsdto "waysbeans/dto/products"
+	dto "waysbeans/dto/result"
+	"waysbeans/models"
+	"waysbeans/repositories"
+
+	// "github.com/cloudinary/cloudinary-go/v2"
+	// "github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+// var path_file = `http://localhost:5000/`
+
+type handlerProduct struct {
+	ProductRepository repositories.ProductRepository
+}
+
+func HandlerProduct(ProductRepository repositories.ProductRepository) *handlerProduct {
+	return &handlerProduct{ProductRepository}
+}
+
+func (h *handlerProduct) FindProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	products, err := h.ProductRepository.FindProducts()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	for i, p := range products {
+		var path_file = os.Getenv("PATH_FILE") + p.Image
+		products[i].Image = path_file
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: products}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) GetProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	var product models.Product
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	product.Image = os.Getenv("PATH_FILE") + product.Image
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: convertResponseProduct(product)}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dataContex := r.Context().Value("dataFile") // add this code
+	filepath := dataContex.(string)             // add this code
+
+	stock, _ := strconv.Atoi(r.FormValue("stock"))
+	price, _ := strconv.Atoi(r.FormValue("price"))
+	request := productsdto.CreateProductRequest{
+		Name:        r.FormValue("name"),
+		Stock:       stock,
+		Price:       price,
+		Description: r.FormValue("description"),
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	// Declare Context Background, Cloud Name, API Key, API Secret ...
+	// var ctx = context.Background()
+	// var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+	// var API_KEY = os.Getenv("API_KEY")
+	// var API_SECRET = os.Getenv("API_SECRET")
+
+	// // Add your Cloudinary credentials ...
+	// cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+
+	// // Upload file to Cloudinary ...
+	// resp, err := cld.Upload.Upload(ctx, filepath, uploader.UploadParams{Folder: "waysbeans"})
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	product := models.Product{
+		Name:        request.Name,
+		Stock:       request.Stock,
+		Price:       request.Price,
+		Description: request.Description,
+		Image:       filepath,
+	}
+
+	product, err = h.ProductRepository.CreateProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	product, _ = h.ProductRepository.GetProduct(product.ID)
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: product}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	dataContex := r.Context().Value("dataFile") // add this code
+	filename := dataContex.(string)             // add this code
+
+	stock, _ := strconv.Atoi(r.FormValue("stock"))
+	price, _ := strconv.Atoi(r.FormValue("price"))
+	request := productsdto.UpdateProductRequest{
+		Name:        r.FormValue("name"),
+		Stock:       stock,
+		Price:       price,
+		Description: r.FormValue("description"),
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	product, _ := h.ProductRepository.GetProduct(id)
+
+	product.Name = request.Name
+	product.Stock = request.Stock
+	product.Price = request.Price
+	product.Description = request.Description
+	product.Image = filename
+
+	product, err = h.ProductRepository.UpdateProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: product}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	deleteProduct, err := h.ProductRepository.DeleteProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: deleteProduct}
+	json.NewEncoder(w).Encode(response)
+}
+
+func convertResponseProduct(u models.Product) productsdto.ProductResponse {
+	return productsdto.ProductResponse{
+		Name:        u.Name,
+		Stock:       u.Stock,
+		Price:       u.Price,
+		Description: u.Description,
+		Image:       u.Image,
+	}
+}
---
+++ handlers/profiles.go
@@ -0,0 +1,213 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strconv"
+	profilesdto "waysbeans/dto/profiles"
+	dto "waysbeans/dto/result"
+	"waysbeans/models"
+	"waysbeans/repositories"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/gorilla/mux"
+)
+
+type handlerProfile struct {
+	ProfileRepository repositories.ProfileRepository
+}
+
+func HandlerProfile(ProfileRepository repositories.ProfileRepository) *handlerProfile {
+	return &handlerProfile{ProfileRepository}
+}
+
+func (h *handlerProfile) FindProfiles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	userInfo := r.Context().Value("userInfo").(jwt.MapClaims)
+	userId := int(userInfo["id"].(float64))
+
+	profiles, err := h.ProfileRepository.FindProfile(userId)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err.Error())
+	}
+
+	for i, p := range profiles {
+		var path_file = os.Getenv("PATH_FILE") + p.Image
+		profiles[i].Image = path_file
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: profiles}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProfile) GetProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	profile, err := h.ProfileRepository.GetProfile(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	profile.Image = os.Getenv("PATH_FILE") + profile.Image
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: convertResponseProfile(profile)}
+	json.NewEncoder(w).Encode(response)
+	return
+}
+
+func (h *handlerProfile) CreateProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	userInfo := r.Context().Value("userInfo").(jwt.MapClaims)
+	userId := int(userInfo["id"].(float64))
+
+	dataContex := r.Context().Value("dataFile")
+	filepath := dataContex.(string)
+
+	request := profilesdto.CreateProfileRequest{
+		Address:  r.FormValue("address"),
+		Postcode: r.FormValue("postcode"),
+		Phone:    r.FormValue("phone"),
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	// // Declare Context Background, Cloud Name, API Key, API Secret ...
+	// var ctx = context.Background()
+	// var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+	// var API_KEY = os.Getenv("API_KEY")
+	// var API_SECRET = os.Getenv("API_SECRET")
+
+	// // Add your Cloudinary credentials ...
+	// cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+
+	// // Upload file to Cloudinary ...
+	// resp, err := cld.Upload.Upload(ctx, filepath, uploader.UploadParams{Folder: "waysbeans"})
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	profile := models.Profile{
+		// Image:    resp.SecureURL,
+		Image:    filepath,
+		Address:  request.Address,
+		Postcode: request.Postcode,
+		Phone:    request.Phone,
+		UserID:   userId,
+	}
+
+	profile, err = h.ProfileRepository.CreateProfile(profile)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	profile, _ = h.ProfileRepository.GetProfile(profile.ID)
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: profile}
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func (h *handlerProfile) UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	dataContex := r.Context().Value("dataFile")
+	filename := dataContex.(string)
+
+	request := profilesdto.UpdateProfileRequest{
+		Address:  r.FormValue("address"),
+		Postcode: r.FormValue("postcode"),
+		Phone:    r.FormValue("phone"),
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	profile, _ := h.ProfileRepository.GetProfile(id)
+
+	profile.Address = request.Address
+	profile.Postcode = request.Postcode
+	profile.Phone = request.Phone
+
+	if filename != "false" {
+		profile.Image = filename
+	}
+
+	profile, err = h.ProfileRepository.UpdateProfile(profile)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: profile}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProfile) DeleteProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	profile, err := h.ProfileRepository.GetProfile(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	deleteProfile, err := h.ProfileRepository.DeleteProfile(profile)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "Success", Data: deleteProfile}
+	json.NewEncoder(w).Encode(response)
+}
+
+func convertResponseProfile(u models.Profile) profilesdto.ProfileResponse {
+	return profilesdto.ProfileResponse{
+		ID:       u.ID,
+		Image:    u.Image,
+		Address:  u.Address,
+		Postcode: u.Postcode,
+		Phone:    u.Phone,
+		UserID:   u.User.ID,
+	}
+}
